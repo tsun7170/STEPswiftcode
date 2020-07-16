9721868cd4c9c55e114b2e249c0bdb40a47ed190
Tsutomu Yoshida <tsuntsunkobi@icloud.com>
2020-07-16 21:12:47 +0900

    initial commit

---
+++ exp2swift/sc_cf.h
@@ -0,0 +1,18 @@
+//
+//  sc_cf.h
+//  STEPcodeMac
+//
+//  Created by Yoshida on 2020/04/12.
+//  Copyright © 2020 Minokamo, Japan. All rights reserved.
+//
+
+#ifndef sc_cf_h
+#define sc_cf_h
+
+#define EXP2CXX
+#define raw		PP_raw
+#define HAVE_DIRENT_H
+#define HAVE_SYS_STAT_H
+#define MAXPATHLEN (1024*2)
+
+#endif /* sc_cf_h */
---
+++ exp2swift/sc_version_string.h
@@ -0,0 +1,14 @@
+//
+//  sc_version_string.h
+//  STEPcodeMac
+//
+//  Created by Yoshida on 2020/04/12.
+//  Copyright © 2020 Minokamo, Japan. All rights reserved.
+//
+
+#ifndef sc_version_string_h
+#define sc_version_string_h
+
+static char sc_version[512] = "v.0.1.0, derived from stepcode v0.8 as of 2019/11/23";
+
+#endif /* sc_version_string_h */
