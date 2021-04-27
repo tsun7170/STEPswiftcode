8a5fcf604988d465363887dd33593114c6f432d4
Tsutomu Yoshida <tsuntsunkobi@icloud.com>
2021-04-28 04:59:31 +0900

    P21 character stream and token stream implemented

--- swiftP21read/main.swift
+++ swiftP21read/main.swift
@@ -7,6 +7,24 @@
 //
 
 import Foundation
+import SwiftSDAIcore
 
-print("Hello, World!")
+print("swiftP21read")
+
+let url = URL(fileURLWithPath: "./TestCases/NIST_CTC_STEP_PMI/nist_ctc_02_asme1_ap242-e2.stp")
+
+let stepsource = try String(contentsOf: url) 
+
+var charstream = stepsource.makeIterator()
+
+//var p21stream = P21Decode.P21CharacterStream(charStream: charstream)
+//
+//while p21stream.lineNumber < 10 {
+//	print(p21stream.next() ?? "<nil>", terminator:"")
+//}
+
+let parser = P21Decode.ExchangeStructureParser(charStream: charstream)
+
+parser.parseExchangeStructure()
+
 
